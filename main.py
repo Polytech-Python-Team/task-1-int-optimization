7b23299f5497d3944155fa4ca6b55e0cd047a946
olexas <alexnevecherin@yandex.ru>
2025-09-24 00:49:17 +0300

    Исправил бин поиск для отрицательных чисел

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 def bin_search(sign=1):
-    l, r = 0, sign * 10 ** 100
-    res = 0
-    while l != r:
+    l = 0 if sign == 1 else -10**100
+    r = 10*100 if sign == 1 else -1
+    while l <= r:
         res = (l + r) // 2
         y = (l + r) // 2
 
         if id(res) == id(y):
-            l = res + sign
+            if sign == 1:
+                l = res + 1
+            else :
+                r = res - 1
         else:
-            r = res - sign
+            if sign == 1:
+                r = res - 1
+            else:
+                l = res + 1
 
-    return res
+    return r if sign == 1 else l
 
 
 M = bin_search(-1)
 N = bin_search()
 
-print(f"Максимальный диапозон значений целых чисел для которых id(a) == id(a), где a - целое число - [{M}, {N}].")
+print(f"Максимальный диапазон значений целых чисел для которых id(a) == id(b), где a == b - [{M}, {N}].")
